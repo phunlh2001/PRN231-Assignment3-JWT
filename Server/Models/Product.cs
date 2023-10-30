7a28f15c986e558211087922b8263d3a9b200213
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-10-30 15:35:48 +0700

    fix: remove attribute required

--- Server/Models/Product.cs
+++ Server/Models/Product.cs
@@ -12,10 +12,8 @@ namespace Server.Models
         [Required(ErrorMessage = "Category must be not empty.")]
         public string Category { get; set; }
         public string Color { get; set; }
-        [Required(ErrorMessage = "Price must be greater than 0.")]
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public decimal UnitPrice { get; set; }
-        [Required(ErrorMessage = "Quantity must be greater than 0.")]
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public int AvailableQuantity { get; set; }
     }
